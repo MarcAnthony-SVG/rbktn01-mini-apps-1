8975b333701d0882fcf137c790cf5204ba794f88
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-12-28 10:44:51 +0100

    server can serve static files

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 6996;
 
-app.get('/', (req, res) => {
-    console.log('hello')
-});
+app.use(express.static('client'));
+
+
 app.listen(port, () => console.log(`Server is up and running on port ${port} `));
