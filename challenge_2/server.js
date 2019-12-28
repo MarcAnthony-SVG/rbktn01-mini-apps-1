8092fd7f05e2d7f6228b2e8677bb16bdabe171a7
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-12-28 10:58:37 +0100

    handling default browser behaviour input in the server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 6996;
 
 app.use(express.static('client'));
+//app.use(bodyParser.urlencoded());
+app.use(express.urlencoded());
 
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
 
 app.listen(port, () => console.log(`Server is up and running on port ${port} `));
