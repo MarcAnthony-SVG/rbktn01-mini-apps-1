a7626adfa5e2336486161d391eca51b3073999e5
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-12-28 16:42:52 +0100

    upload files to the server functionality

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helperFunctions = require('./utilties/helpers');
+const formidable = require('formidable');
 
 const Promise = require('bluebird');
 const fs = require('fs');
@@ -19,32 +20,68 @@ app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/', (req, res) => {
-    console.log(req.body);
-    var csv = jsonToCSV(JSON.parse(req.body.cInput));
-    readFileAsync('./utilties/ids.txt')
-        .then(counter => Promise.resolve(Number(counter)))
-        .catch(() => {
-            return writeFileAsync('./utilties/ids.txt', 0).then(() => 0)
+    new formidable.IncomingForm().parse(req)
+        .on('field', (name, field) => {
+            var csv = jsonToCSV(JSON.parse(field));
+            readFileAsync('./utilties/ids.txt')
+                .then(counter => Promise.resolve(Number(counter)))
+                .catch(() => {
+                    return writeFileAsync('./utilties/ids.txt', 0).then(() => 0)
+                })
+                .then(id => {
+                    return writeFileAsync(`${__dirname}/utilties/${id}.csv`, csv)
+                        .then(() => Promise.resolve(id))
+                })
+                .catch(() => console.log('Error while writing the file'))
+                .then((n = 0) => {
+                    res.header('Content-Disposition', `attachment; filename=report${n}.csv`);
+                    res.sendFile(path.join(__dirname, `/utilties/${n}.csv`));
+                    return Promise.resolve(n);
+                })
+                .then(id => {
+                    writeFileAsync(`./utilties/ids.txt`, id + 1);
+                    return Promise.resolve(id + 1)
+                })
+                .catch(() => console.log('error occurred'))
         })
-        .then(id => {
-            return writeFileAsync(`${__dirname}/utilties/${id}.csv`, csv)
-                .then(() => Promise.resolve(id))
+        .on('fileBegin', (name, file) => {
+            file.path = __dirname + '/uploads' + file.name;
         })
-        .catch(() => console.log('Error while writing the file'))
-        .then((n = 0) => {
-            res.header('Content-Disposition', `attachment; filename=report${n}.csv`);
-            res.sendFile(path.join(__dirname, `/utilties/${n}.csv`));
-            return Promise.resolve(n);
+        .on('file', (name, file) => {
+            console.log('Uploaded file', name, file)
         })
-        .then(id => {
-            writeFileAsync(`./utilties/ids.txt`, id + 1);
-            return Promise.resolve(id + 1)
+        .on('aborted', () => {
+            console.error('Request aborted by the user')
         })
-        .then(lastId => {
-            writeFileAsync(`./utilties/last.txt`, lastId );
-            return Promise.resolve(lastId)
+        .on('error', (err) => {
+            console.error('Error', err);
+            throw err
         })
-        .catch(() => console.log('error occurred'))
+    // var csv = jsonToCSV(JSON.parse(req.body.cInput));
+    // readFileAsync('./utilties/ids.txt')
+    //     .then(counter => Promise.resolve(Number(counter)))
+    //     .catch(() => {
+    //         return writeFileAsync('./utilties/ids.txt', 0).then(() => 0)
+    //     })
+    //     .then(id => {
+    //         return writeFileAsync(`${__dirname}/utilties/${id}.csv`, csv)
+    //             .then(() => Promise.resolve(id))
+    //     })
+    //     .catch(() => console.log('Error while writing the file'))
+    //     .then((n = 0) => {
+    //         res.header('Content-Disposition', `attachment; filename=report${n}.csv`);
+    //         res.sendFile(path.join(__dirname, `/utilties/${n}.csv`));
+    //         return Promise.resolve(n);
+    //     })
+    //     .then(id => {
+    //         writeFileAsync(`./utilties/ids.txt`, id + 1);
+    //         return Promise.resolve(id + 1)
+    //     })
+    //     .then(lastId => {
+    //         writeFileAsync(`./utilties/last.txt`, lastId );
+    //         return Promise.resolve(lastId)
+    //     })
+    //     .catch(() => console.log('error occurred'))
 
 });
 
