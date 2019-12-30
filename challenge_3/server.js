ba31ec249abb19ee1dff86df5e155e13d36df652
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-12-30 10:16:20 +0100

    setup basic html and imported react and babel

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+
+var app = express();
+const port = 6969;
+
+app.use(express.static('public'));
 
 
+
+
+
+
+
+app.listen(port, () => console.log(`server is running on port  ${port}`));
