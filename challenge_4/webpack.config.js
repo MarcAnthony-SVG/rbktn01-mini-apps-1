3a81bc0e2948b9c2c76e6da5432ea5c8f55588cd
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-12-31 14:34:01 +0100

    setting up webpack

---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+    entry: './client/index.jsx',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, '/client/dist')
+    }
+};
